1a1a48ce85db719d47d3ab024818006b19e2c930
nishilv2 <704990825@qq.com>
2015-05-17 20:02:52 +0800

    重新整合函数代码

---
+++ Printer/Printer/API.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+namespace NKprint2
+{
+    class API
+    {
+        public static string token = "";
+        public static int myPage=1;
+        private static string server_url = Program.serverUrl;
+        public static string PutMethod(string metodUrl, string para, Encoding dataEncode)
+        {
+            string down = server_url + metodUrl;
+            //string down = @"http://yunyin.org/api.php/Index/put";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(down);
+            if (token != "")
+            {
+                request.Headers.Add("Token", token);
+            }
+            request.Method = "PUT";
+            string s = "1";
+            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            request.Accept = "Accept: application/json";
+            try
+            {
+                //byte[] byteArray = dataEncode.GetBytes(para); //转化
+                //request.ContentType = "application/x-www-form-urlencoded";
+                //request.Accept = "Accept: application/json";
+                //request.ContentLength = byteArray.Length;
+                //Stream newStream = request.GetRequestStream();
+                //newStream.Write(byteArray, 0, byteArray.Length);//写入参数
+                //newStream.Close();
+
+                //Console.WriteLine("\nThe HttpHeaders are \n\n\tName\t\tValue\n{0}", request.Headers);
+                //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                //StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.Default);
+                //s = sr.ReadToEnd();
+                //sr.Close();
+                //response.Close();
+                //newStream.Close();
+                using (StreamWriter writer = new StreamWriter(request.GetRequestStream()))
+                {
+                    writer.Write(para);
+                }
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    //while (reader.Peek() != -1)
+                    //{
+                    //    Console.WriteLine(reader.ReadLine());
+                    //}
+                    s = reader.ReadToEnd();
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return s;
+            
+        }
+
+        public static string DeleteMethod(string metodUrl)
+        {
+            string s = "1";
+
+            string url = server_url + metodUrl;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Accept = "Accept: Application/json";
+            request.Method = "delete";
+            HttpWebResponse response = null;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException e)
+            {
+                response = (HttpWebResponse)e.Response;
+                MessageBox.Show(e.Message + " - " + getRestErrorMessage(response));
+                return default(string);
+            }
+            return s;
+        }
+
+        //GetMEthod用来完成Accept: application/json
+        /// <summary>
+        /// GetMEthod用来完成Accept: application/json(string metodUrl)
+        /// </summary>
+        /// <param name="metodUrl"></param>
+        /// <returns></returns>
+        public static string GetMethod(string metodUrl)
+        {
+            string down = server_url  + metodUrl;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(down);
+            if (token!="")
+            {
+                request.Headers.Add("Token", token);//修改Headers,添加
+            }
+            request.Method = "get";
+            request.Accept = "Accept: application/json";
+            //request.ContentType = "application/json;charset=UTF-8";
+            HttpWebResponse response = null;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException e)
+            {
+                response = (HttpWebResponse)e.Response;
+                MessageBox.Show(e.Message + " - " + getRestErrorMessage(response));
+                return default(string);
+            }
+            //
+            //Console.WriteLine("\nThe HttpHeaders are \n\n\tName\t\tValue\n{0}", request.Headers);
+            string json = getResponseString(response);
+            return json;
+        }
+
+        // REST @GET 方法，根据泛型自动转换成实体，支持List<T>
+        /// <summary>
+        /// REST @GET 方法，根据泛型自动转换成实体，支持List<T>(string metodUrl)
+        /// </summary>
+        /// <param name="metodUrl"></param>
+        /// <returns></returns>
+        public static string doGetMethodToObj(string metodUrl)
+        {
+            //get获取。/api.php/File/?page=2&token=....
+            string down = server_url + metodUrl;
+            //string down = server_url + @"/File/?token=" + metodUrl;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(down);
+            request.Method = "get";
+            request.Accept = "Accept:application/json";
+            request.ContentType = "application/json;charset=UTF-8";
+            HttpWebResponse response = null;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException e)
+            {
+                response = (HttpWebResponse)e.Response;
+                MessageBox.Show(e.Message + " - " + getRestErrorMessage(response));
+                return default(string);
+            }
+            
+            string json = getResponseString(response);
+            return json;
+        }
+        
+        //post方法来从服务器访问数据
+        /// <summary>
+        /// post方法来从服务器访问数据
+        /// </summary>
+        /// <param name="postUrl"></param>
+        /// <param name="paramData"></param>
+        /// <param name="dataEncode"></param>
+        /// <returns></returns>
+        public static string PostMethod(string postUrl, string paramData, Encoding dataEncode)
+        {
+            postUrl = server_url + postUrl;
+            string ret = string.Empty;
+            try
+            {
+                byte[] byteArray = dataEncode.GetBytes(paramData); //转化
+                HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(postUrl));
+                webReq.Method = "POST";
+                webReq.ContentType = "application/x-www-form-urlencoded";
+                webReq.Accept = "Accept: application/json";
+                webReq.ContentLength = byteArray.Length;
+                Stream newStream = webReq.GetRequestStream();
+                newStream.Write(byteArray, 0, byteArray.Length);//写入参数
+                newStream.Close();
+                HttpWebResponse response = (HttpWebResponse)webReq.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.Default);
+                ret = sr.ReadToEnd();
+                sr.Close();
+                response.Close();
+                newStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return ret;
+        }
+        private static string getResponseString(HttpWebResponse response)
+        {
+            string json = null;
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.GetEncoding("UTF-8")))
+            {
+                json = reader.ReadToEnd();
+            }
+            return json;
+        }
+        // 获取异常信息
+        /// <summary>
+        /// 获取异常信息
+        /// </summary>
+        /// <param name="errorResponse"></param>
+        /// <returns></returns>
+        private static string getRestErrorMessage(HttpWebResponse errorResponse)
+        {
+            string errorhtml = getResponseString(errorResponse);
+            string errorkey = "spi.UnhandledException:";
+            errorhtml = errorhtml.Substring(errorhtml.IndexOf(errorkey) + errorkey.Length);
+            errorhtml = errorhtml.Substring(0, errorhtml.IndexOf("\n"));
+            return errorhtml;
+        }
+    }
+}
---
+++ Printer/Printer/datatype.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace NKprint2
+{
+   
+        //我们定义的JSON数据结构 
+        //属性的名字，必须与json格式字符串中的"key"值一样。
+        public class ToJsonMy
+        {
+            private string m_double_side;
+            private string m_copies;
+            private string m_status;
+
+            public string id { get; set; }//文件编号
+            public string use_id { get; set; }//
+            public string use_name { get; set; }//
+            public string student_number { get; set; }//
+            public string pri_id { get; set; }
+            public string name { get; set; }
+            public string url { get; set; }
+            public string time { get; set; }
+            public string lujing { get; set; }
+
+            public string copies
+            {
+                get
+                {
+                    if (m_copies == "0")
+                    {
+                        return "现场打印";
+                    }
+                    else
+                        return m_copies + "份";
+                }
+                set { m_copies = value; }
+
+            }
+
+            public string double_side
+            {
+                get
+                {
+                    if (m_double_side == "0")
+                    {
+                        return "单面";
+                    }
+                    else
+                        return "双面";
+                }
+                set { m_double_side = value; }
+            }
+            //根据属性的特点，将存储的和得到的status建立起映射关系；
+            public string status
+            {
+                get
+                {
+                    switch (m_status)
+                    {
+                        case "1":
+                            return "未下载";
+                        case "2":
+                        case "download":
+                            return "已下载";
+                        case "3":
+                        case "printing":
+                            return "正打印";
+                        case "4":
+                        case "printed":
+                            return "已打印";
+                        case "5":
+                        case "payed":
+                            return "已付款";
+                    }
+                    return "0";
+                }
+                set
+                {
+                    m_status = value;
+                }
+            }
+            public string color { get; set; }
+            public string ppt_layout { get; set; }
+            public string requirements { get; set; }
+            public string strcolor
+            {
+                get
+                {
+                    if (color == "0")
+                        return "黑白";
+                    else
+                        return "彩印";
+                }
+                set
+                {
+                    strcolor = value;
+                }
+            }
+            public string ppt
+            {
+                get
+                {
+                    switch (ppt_layout)
+                    {
+                        case "0":
+                            return "-";
+                        case "1":
+
+                            return "1X1";
+                        case "2":
+
+                            return "2X3";
+                        case "3":
+
+                            return "2X4";
+                        case "4":
+
+                            return "3X3";
+                    }
+                    return "0";
+                }
+                set
+                {
+                    ppt = value;
+                }
+            }
+
+                    
+                    
+        }
+        //定义获取api的token
+        public struct ToMyToken
+        {
+            public string token { get; set; }
+            public string name { get; set; }
+            public string id { get; set; }
+            public float version { get; set; }
+        }
+        //声明用户的隐私信息
+        //{
+        //    "id": "用户id",
+        //    "name": "姓名",
+        //    "student_number": "学号",
+        //    "gender": "性别",
+        //    "phone": "真实手机号",
+        //    "email": "真实邮箱",
+        //    "status": "用户状态标识码"
+        // }
+        public class userInfo
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public string student_number { get; set; }
+            public string gender { get; set; }
+            public string phone { get; set; }
+            public string email { get; set; }
+            public string status { get; set; }
+        }
+
+
+    
+}
